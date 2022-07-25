d2268314b560dd8f0639dceb0611daa8cb5de538
铁板烤鲈鱼 <2708702597@qq.com>
2022-07-25 02:24:19 +0000

    update main.py. 整理代码

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ def geshihua(shu):
 
 #分数模块
 class fenshu:
-    def __init__(self,zi=0,mu=1):#初始化
-         
+    def __init__(self,zi=0,mu=1):#初始化        
         if bigeshi(zi,0.1):#float格式
             self.zi=zi*(10**15)
             self.mu=(10**15)                    
@@ -60,7 +59,7 @@ class fenshu:
     def __float__(self):
         return self.zhi    
     __call__=__float__
-    __int__=lambda self:int(self.zi)
+    __int__=lambda self:int(self.zhi)
 
 
     #比较      
@@ -78,28 +77,28 @@ class fenshu:
         shu=geshihua(shu)#格式化，兼容int和float
         zi=self.zi*shu.mu+shu.zi*self.mu
         mu=self.mu*shu.mu    
-        a=fenshu(zi,mu)#包装       
+        a=fenshu(zi,mu)     
         return a
         
     def __sub__(self,shu):#减
-        shu=geshihua(shu)#格式化，兼容int和float
+        shu=geshihua(shu)#兼容
         zi=self.zi*shu.mu-shu.zi*self.mu
         mu=self.mu*shu.mu
-        a=fenshu(zi,mu)#包装
-        
+        a=fenshu(zi,mu)      
         return a
      
     def __mul__(self,shu):#乘
-        shu=geshihua(shu)#格式化，兼容int和float
+        shu=geshihua(shu)#兼容
         zi=self.zi*shu.zi      
         mu=self.mu*shu.mu
-        a=fenshu(zi,mu)#包装       
+        a=fenshu(zi,mu)   
         return a
+   
     def __truediv__(self,shu):#除
-        shu=geshihua(shu)#格式化，兼容int和float
+        shu=geshihua(shu)#兼容
         zi=self.zi*shu.mu     
         mu=self.mu*shu.zi
-        a=fenshu(zi,mu)#包装        
+        a=fenshu(zi,mu)#       
         return a
         
     __div__=__floordiv__=__truediv__#n种除法    
@@ -139,8 +138,7 @@ class fenshu:
 #实数             
 class shishu:
     
-    def __init__(self,shu=0,dishu=1):
-        
+    def __init__(self,shu=0,dishu=1):      
         if shu!=0 and bigeshi(shu,shishu()):#实数格式，shu!=1是为了防止无限递推
             self.xinxi=deepcopy(shu.xinxi)
             return
@@ -152,9 +150,7 @@ class shishu:
             self.xinxi={}
             self.xinxi[dishu]=shu
         a=self.huajian()
-        self.xinxi.clear()
-        
-        
+        self.xinxi.clear()       
         for i in a:
             self.xinxi[i]=a[i]
                 
@@ -164,7 +160,8 @@ class shishu:
             self.xinxi[beikaifangshu]+=geshihua(shu)
         else:                                        #不存在
             self.xinxi[beikaifangshu]=geshihua(shu)
-    #三则运算（除法有bug）
+    #四则运算（除法有bug）
+    
     #加法    
     def __add__(self,shu):
         linshi=shishu(self)
@@ -176,6 +173,7 @@ class shishu:
             shu=geshihua(shu)    
             linshi.jiashu(shu)
         return linshi
+    
     #减法    
     def __sub__(self,shu):
         linshi=shishu(self)#复制一份       
@@ -186,6 +184,7 @@ class shishu:
             shu=geshihua(shu*(-1))    
             linshi.jiashu(shu)
         return linshi
+    
     def huajian(self):#化简
         def fenjie(a):
             b=1
@@ -215,7 +214,7 @@ class shishu:
                 del fuzhi2[linshi]
         return fuzhi2          
        
-    def __mul__(self,shu):
+    def __mul__(self,shu):#乘法
         linshi=shishu()
         xinxi1=self.xinxi
         xinxi2=shu.xinxi
@@ -223,7 +222,9 @@ class shishu:
             for yi in xinxi2:
                 linshi.jiashu(xinxi1[jia]*xinxi2[yi],beikaifangshu=jia*yi)          
         return shishu(linshi.huajian())       
-    def __truediv__(self,shu):#有BUG
+    def __truediv__(self,shu):#除法有BUG
+        if len(list(shu))>4:
+            raise Exception("理论问题，除数项数不得大于4")
         a=self
         b=shu    
         if (b.xinxi.get(1) is None):
@@ -239,10 +240,8 @@ class shishu:
         for i in a.xinxi:
             c.xinxi[i]=a.xinxi[i]/b.xinxi[1]    
         return c    
+    
     #文本化    
-    __str__=__repr__=(lambda self : 
-    	                "shishu"+str(self.xinxi).
-    	                replace("{","[").
-                    	replace("}","]")  
-    	             )         
-
+    __str__=(lambda self : "shishu"+str(self.xinxi).
+    	                replace("{","[").replace("}","]") ) 
+    __repr__=(lambda self : "shishu("+str(self.xinxi)+")")
