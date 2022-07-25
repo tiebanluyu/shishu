7c53abcd167a04448906c0a72f4eb04b5f590f3e
铁板烤鲈鱼 <2708702597@qq.com>
2022-07-25 03:38:17 +0000

    update pi.py.

--- pi.py
+++ pi.py
@@ -1,5 +1,5 @@
 def pi(num):
-    pi=fenshu()
+    Pi=fenshu()  
     for i in range(num):
-        pi+=fenshu(4,(2*i-1)*(-1)**(i+1))
-    return pi
+        Pi+=fenshu(4,(2*i+1)*(-1)**(i))
+    return Pi    
