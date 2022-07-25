6eb39c8314008dd6a1ac605730a9178e55a72263
铁板烤鲈鱼 <2708702597@qq.com>
2022-07-25 04:44:23 +0000

    update pi.py.

--- pi.py
+++ pi.py
@@ -1,11 +1,12 @@
-def pi_1(num):
+def pi_f1(num):
     Pi=fenshu()  
     for i in range(num):
         Pi+=fenshu(4,(2*i+1)*(-1)**(i))
     return Pi    
-    #CalPiV1.py
-def pi_2(num):
+    
+def pi_f2(num):
     pi = fenshu()
     for k in range(num):
         pi += fenshu(1,pow(16, k) * (4 / (8 * k + 1) - 2 /(8 * k + 4) - 1/(8 * k + 5) - 1 /(8 * k + 6)))
     return pi
+pi=fenshu(355,113)
