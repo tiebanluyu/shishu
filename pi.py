b9c3c36dae6f31dcd65a9b08552e6ec4a89f8426
铁板烤鲈鱼 <2708702597@qq.com>
2022-08-03 03:08:05 +0000

    update pi.py.

--- pi.py
+++ pi.py
@@ -1,14 +1,14 @@
 class Pi:
     @staticmethod
     def f1(num):
-        Pi=fenshu()  
+        Pi=Fac()  
         for i in range(num):
-            Pi+=fenshu(4,(2*i+1)*(-1)**(i))
+            Pi+=Fac(4,(2*i+1)*(-1)**(i))
         return Pi    
     @staticmethod
     def f2(num):
-        pi = fenshu()
+        pi = Fac()
         for k in range(num):
-            pi += fenshu(1,pow(16, k) * (4 / (8 * k + 1) - 2 /(8 * k + 4) - 1/(8 * k + 5) - 1 /(8 * k + 6)))
+            pi += Fac(1,pow(16, k) * (4 / (8 * k + 1) - 2 /(8 * k + 4) - 1/(8 * k + 5) - 1 /(8 * k + 6)))
         return pi
-    pi=fenshu(355,113)
+    pi=Fac(355,113)
